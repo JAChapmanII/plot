576561c5a534082d874e086deb8dbf992c42ae49
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-01 14:43:24 -0400

    Change plot-test to be project 1 for ANMI

--- plot-test.c
+++ plot-test.c
@@ -2,20 +2,59 @@
 #include <stdio.h>
 
 double f1(double x) {
-	return (x + 3) / (x*x + 5);
+	return x*x;
 }
 
 int main(int argc, char **argv) {
-	Interval xi = { -5, 5 }, yi = getYInterval_fYofX(f1, xi);
-	double res = 3;
+	int n;
+	double x[13], y[13], h = 0.1;
 
-	for(; res >= 0.01; res -= .01) {
-		setPlotResolution(res);
-		clearPlot();
-		drawAxes(xi, yi);
-		plot_fYofX(f1, xi, yi);
-		plotDelay(50);
+	setPlotXInterval(0.0, 5.0);
+	setPlotYInterval(0.0, 25.0);
+
+	setPlotResolution(1);
+	clearPlot();
+	drawAxes();
+	plot_fYofX(f1);
+	pause();
+
+	printf("res: 0.5\n");
+	setPlotResolution(0.5);
+	clearPlot();
+	drawAxes();
+	plot_fYofX(f1);
+	pause();
+
+	printf("res: 0.1\n");
+	setPlotResolution(0.1);
+	clearPlot();
+	drawAxes();
+	plot_fYofX(f1);
+	pause();
+
+	printf("res: 0.01\n");
+	setPlotResolution(0.01);
+	clearPlot();
+	drawAxes();
+	plot_fYofX(f1);
+	pause();
+
+	printf("approx vs exact\n");
+	setPlotXInterval(0.0, 1.0);
+	setPlotYInterval(0.0, 1.0);
+
+	setPlotResolution(0.01);
+	clearPlot();
+	drawAxes();
+	plot_fYofX(f1);
+
+	x[0] = 0; y[0] = 0;
+	for(n = 0; n < 13; ++n) {
+		x[n+1] = n * h;
+		y[n+1] = y[n] + h * 2 * x[n];
+		drawDot(x[n], y[n]);
 	}
+	pause();
 
 	return 0;
 }
